fadac1fc35fb13c8957b151aad20d69ff5471181
tyfah <tafadzwaallentyfah@gmail.com>
2021-08-12 14:10:32 +0200

    Controller

--- src/main/java/zw/co/afrosoft/controller/RegistrationController.java
+++ src/main/java/zw/co/afrosoft/controller/RegistrationController.java
@@ -1,5 +1,6 @@
 package zw.co.afrosoft.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(path = "api/v1/registration")
 public class RegistrationController {
+
     private RegistrationService registrationService;
 
+    @PostMapping
     public String register(@RequestBody RegistrationRequest request){
         return registrationService.register(request);
     }
